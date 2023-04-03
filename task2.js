e30b00f2cc932b935ecd37226bf5f5a5d9aa3250
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-04-03 23:31:41 +0600

    Файл js, который меняет элементам страницы цвет

---
+++ task2.js
@@ -0,0 +1,7 @@
+const element = document.getElementsByClassName('element')
+element[0].style.color = 'red'
+element[1].style.color = 'red'
+element[2].style.color = 'red'
+element[3].style.color = 'green'
+element[4].style.color = 'green'
+element[5].style.color = 'green'
